02714b6c87a1c278959cac57f78e523d4b15184d
Sabrina C. Sales <sacsales@icloud.com>
2023-07-10 10:35:49 +0400

    Initial Commit

---
+++ JokesApp/JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Sabrina C. Sales on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
